638a3cecd643397d8fcfa1337353ae6c5ed5a444
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2025-01-29 18:52:14 +0530

    React introduntion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import "./App.css";
-import Welcome from "./components/Welcome";
+import { useState } from "react";
 
-function App() {
-  return <Welcome />;
+function App2() {
+  // let count = 0;
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/*  */}
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          // count = count + 1;
+          // console.log(count);
+        }}
+      >
+        Increment
+      </button>
+    </>
+  );
 }
 
-export default App;
+export default App2;
